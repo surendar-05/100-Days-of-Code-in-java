c86dc57f1a9e7e129d2a0a05cd9b717286e25960
surendar-05 <103345824+surendar-05@users.noreply.github.com>
2023-04-04 19:58:31 +0530

    Day1 - Addition of two numbers
    Write a Java program to perform the addition of 2 integer numbers.

---
+++ Day -1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Sum2numbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int number1=sc.nextInt();
+        int number2=sc.nextInt();
+        System.out.println(number1+number2);
+        
+    }
+}
